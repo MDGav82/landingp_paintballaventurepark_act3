617f52371588fd167d790fa05d27fe14c7f0b67a
MDGav82 <valentin.aviles@my-digital-school.org>
2025-12-10 21:22:11 +0100

    v3 of app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 👇 AJOUTEZ JUSTE CETTE LIGNE ICI
+      base: '/landingp_paintballaventurepark_act3/',
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
